46b6fb9e32a50468a7cb68192689aeb93882ba96
ibraah tech <ezramatu9@gmail.com>
2024-07-23 17:22:56 +0300

    bmw

--- index.js
+++ index.js
@@ -87,17 +87,17 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./sessions`)
         const { connection, lastDisconnect } = s
         if (connection == "open") {
             await delay(1000 * 10)
-            await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `🪀Support/Contact Developer\n\n\n⎆Donate: https://telegra.ph/file/0c225f7da5616cdcbec80.jpg\n\n⎆YouTube: https://youtube.com/ibrahimaitech\n\n⎆Telegram Channel: https://t.me/xeonbotinc\n\n⎆Telegram Chat: https://t.me/+AYOyJflnt-AzNGFl\n\n⎆WhatsApp Gc1: https://chat.whatsapp.com/Kjm8rnDFcpb04gQNSTbW2d\n\n⎆WhatsApp Gc2: https://chat.whatsapp.com/EEOnU0V7dl9HF1mMFO8QWa\n\n⎆WhatsApp Gc3: https://chat.whatsapp.com/Dh0lD0Ee5hN1JMFXNqtxSG\n\n⎆WhatsApp Pm: Wa.me/916909137213\n\n⎆Instagram: https://instagram.com/unicorn_xeon13\n\n⎆GitHub: https://github.com/DGXeon/\n\n⎆Blog: https://dreamguyxeonfiles.blogspot.com/2022/05/bots%20whatsapp%20mods.html?m=1\n\n\n` });
+            await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `🪀Support/Contact Developer\n\n\n⎆Donate: https://telegra.ph/file/0c225f7da5616cdcbec80.jpg\n\n⎆YouTube: https://youtube.com/ibrahimaitech\n\n⎆Telegram Channel: https://t.me/ibrahimtechai\n\n⎆WhatsApp Channel: https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y\n\n⎆WhatsApp Pm: Wa.me/25471077266\n\n⎆Instagram: https://instagram.com/ibrahim_adams_official\n\n⎆GitHub: https://github.com/ibrahimaitech/\n\n⎆Blog: https://dreamguyxeonfiles.blogspot.com/2022/05/bots%20whatsapp%20mods.html?m=1\n\n\n` });
             let sessionXeon = fs.readFileSync('./sessions/creds.json');
             await delay(1000 * 2) 
              const xeonses = await  XeonBotInc.sendMessage(XeonBotInc.user.id, { document: sessionXeon, mimetype: `application/json`, fileName: `creds.json` })
              await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `⚠️Do not share this file with anybody⚠️\n
 ┌─❖
-│ Ohayo 😽
+│ Hello
 └┬❖  
-┌┤✑  Thanks for using X-PairCode
+┌┤✑  Thanks for using ʙᴍᴡ ᴍᴅ
 │└────────────┈ ⳹        
-│©2020-2023 XeonBotInc 
+│ > © sɪʀ ɪʙʀᴀʜɪᴍ
 └─────────────────┈ ⳹\n\n ` }, {quoted: xeonses});
               await delay(1000 * 2) 
               process.exit(0)
